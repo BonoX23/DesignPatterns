4a3a6deb186e0c9fce212e13656c3878caaf3a87
Bonox23 <ricardo.bonos@gmail.com>
2023-06-06 01:06:24 -0300

    Implementação Método Observer

--- DesignPatterns/DesignPatterns/MethodChaining/NotaFiscalBuilder.cs
+++ DesignPatterns/DesignPatterns/MethodChaining/NotaFiscalBuilder.cs
@@ -3,11 +3,47 @@ using DesignPatterns.Serviços;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DesignPatterns.MethodChaining
 {
+    public interface IAcaoAposGerarNota
+    {
+        void Executa(NotaFiscal notaFiscal);
+    }
+
+    public class EnviadorDeEmail : IAcaoAposGerarNota
+    {
+        public void Executa(NotaFiscal notaFiscal)
+        {
+            Console.WriteLine("enviando por e-mail");
+        }
+    }
+
+    public class NotaFiscalDao : IAcaoAposGerarNota
+    {
+        public void Executa(NotaFiscal notaFiscal)
+        {
+            Console.WriteLine("salvando no banco");
+        }
+    }
+
+    public class Multiplicador : IAcaoAposGerarNota
+    {
+        public double Fator { get; set; }
+
+        public Multiplicador(double fator)
+        {
+            Fator = fator;
+        }
+        public void Executa(NotaFiscal notaFiscal)
+        {
+            Console.WriteLine(notaFiscal.ValorTotal * Fator);  
+        }
+    }
+
     public class NotaFiscalBuilder
     {
         public string RazaoSocial { get; private set; }
@@ -18,22 +54,37 @@ namespace DesignPatterns.MethodChaining
         public string Observacoes { get; private set; }
 
         public IList<ItemDaNota> TodosItens = new List<ItemDaNota>();
+
+        public IList<IAcaoAposGerarNota> TodasAsAcoesASeremExecutas;
+
+        public NotaFiscalBuilder()
+        {
+            this.TodasAsAcoesASeremExecutas = new List<IAcaoAposGerarNota>();
+        }
+
+        public void AdicionaAcao(IAcaoAposGerarNota novaAcao)
+        {
+            this.TodasAsAcoesASeremExecutas.Add(novaAcao);
+        }
+
         public NotaFiscalBuilder ParaEmpresa(string razaoSocial)
         {
             this.RazaoSocial = razaoSocial;
             return this;
         }
+
         public NotaFiscalBuilder NaDataAtual()
         {
             this.Data = DateTime.Now;
-
             return this;
         }
+
         public NotaFiscalBuilder ComCnpj(string cnpj)
         {
             this.Cnpj = cnpj;
             return this;
         }
+
         public NotaFiscalBuilder ComItem(ItemDaNota item)
         {
             TodosItens.Add(item);
@@ -51,14 +102,26 @@ namespace DesignPatterns.MethodChaining
 
         public NotaFiscal Constroi()
         {
-            return new NotaFiscal(RazaoSocial, Cnpj, Data, ValorTotal,
+            NotaFiscal notaFiscal = new NotaFiscal(RazaoSocial, Cnpj, Data, ValorTotal,
                                 Impostos, TodosItens, Observacoes);
+
+            foreach(IAcaoAposGerarNota acao in TodasAsAcoesASeremExecutas)
+            {
+                acao.Executa(notaFiscal);
+            }
+
+            return notaFiscal;
         }
 
         class Teste
         {
             public static void Main(String[] args)
             {
+                NotaFiscalBuilder builder = new NotaFiscalBuilder();
+                builder.AdicionaAcao(new EnviadorDeEmail());
+                builder.AdicionaAcao(new NotaFiscalDao());
+                builder.AdicionaAcao(new Multiplicador(4.4));
+
                 NotaFiscal nf = new NotaFiscalBuilder().ParaEmpresa("Caelum")
                                   .ComCnpj("123.456.789/0001-10")
                                   .ComItem(new ItemDaNota("item 1", 100.0))
